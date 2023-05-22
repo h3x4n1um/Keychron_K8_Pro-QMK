1be9b3782bbe08231a7064e9535fe6adbbb015e6
Fae <faenkhauser@gmail.com>
2023-05-22 15:19:27 -0500

    Add LED freeing

--- quantum/xap/handlers/lighting.c
+++ quantum/xap/handlers/lighting.c
@@ -176,39 +176,68 @@ bool xap_respond_save_rgb_matrix_config(xap_token_t token, const void *data, siz
 #endif // XAP_DIRECT_DISABLE_UPPER LIMIT
 
 static bool flags_saved = false;
+// Rounding up math: ((input + (num - 1)) / num)
 #define FLAG_COUNT ((RGB_MATRIX_LED_COUNT + (2 - 1)) / 2)
 // Assigning LED_FLAG_NONE doesn't make a lot of sense since each uint8_t is more than 1 flag
 uint8_t stored_flags[FLAG_COUNT] = {0};
+
+//#define FLAG_SAVED ((RGB_MATRIX_LED_COUNT + (8 - 1)) / 8)
+//uint8_t flags_saved[FLAG_SAVED] = {0};
 
 #define BITMASK_MODIFIER 0
 #define BITMASK_UNDERGLOW 1
 #define BITMASK_KEYLIGHT 2
 #define BITMASK_INDICATOR 3
 
+/* TODO:
+    Add way to release single LEDs from Direct modes
+    Similar structure to setting them (DONE)
+
+    Figure out how to gracefully use indicator functions outside of calling them manually
+    Add way to free LEDs from keyboard
+*/
 bool xap_respond_direct_free(xap_token_t token, const void *data, size_t length) {
-    if (flags_saved) {
-        for (int led_ndx = 0; led_ndx < RGB_MATRIX_LED_COUNT; led_ndx++) {
-            int flag_ndx = led_ndx / 2;
-            // Section is the starting bit index for the flags to be stored at
-            // 0..3 is the first LED, and 4..7 is the second LED
-            int section = ((led_ndx % 2 == 0) ? 0 : 4); 
-            if ((1 << (section + BITMASK_MODIFIER )) & stored_flags[flag_ndx])
-              g_led_config.flags[led_ndx] += LED_FLAG_MODIFIER;
-            if ((1 << (section + BITMASK_UNDERGLOW)) & stored_flags[flag_ndx])
-              g_led_config.flags[led_ndx] += LED_FLAG_UNDERGLOW;
-            if ((1 << (section + BITMASK_KEYLIGHT)) & stored_flags[flag_ndx])
-              g_led_config.flags[led_ndx] += LED_FLAG_KEYLIGHT;
-            if ((1 << (section + BITMASK_INDICATOR)) & stored_flags[flag_ndx])
-              g_led_config.flags[led_ndx] += LED_FLAG_INDICATOR;
-            if (g_led_config.flags[led_ndx] == (LED_FLAG_MODIFIER + LED_FLAG_UNDERGLOW + LED_FLAG_KEYLIGHT + LED_FLAG_INDICATOR)) {
-              g_led_config.flags[led_ndx] = LED_FLAG_ALL;
-            }
-            if (section == 4) {
-                stored_flags[flag_ndx] = 0;
-            }
+    xap_route_lighting_rgb_matrix_direct_free_arg_t* args = (xap_route_lighting_rgb_matrix_direct_free_arg_t*)data;
+    int starting_led = args->starting_led;
+    int led_count = args->led_count;
+    bool free_all = args->free_all;
+
+    if ((starting_led < 0) || (starting_led >= RGB_MATRIX_LED_COUNT))
+        return false;
+    
+    if ((led_count <= 0) || ((starting_led + led_count) > RGB_MATRIX_LED_COUNT))
+        return false;
+
+    if (!flags_saved)
+        return false;
+
+    if (free_all)
+        starting_led = 0;
+    
+    int ending_led = (free_all ? RGB_MATRIX_LED_COUNT : (starting_led + led_count));
+
+    for (int led_ndx = starting_led; led_ndx < ending_led; led_ndx++) {
+        int flag_ndx = led_ndx / 2;
+        // Section is the starting bit index for the flags to be stored at
+        // 0..3 is the first LED, and 4..7 is the second LED
+        int section = ((led_ndx % 2 == 0) ? 0 : 4);
+        if ((1 << (section + BITMASK_MODIFIER )) & stored_flags[flag_ndx])
+          g_led_config.flags[led_ndx] += LED_FLAG_MODIFIER;
+        if ((1 << (section + BITMASK_UNDERGLOW)) & stored_flags[flag_ndx])
+          g_led_config.flags[led_ndx] += LED_FLAG_UNDERGLOW;
+        if ((1 << (section + BITMASK_KEYLIGHT)) & stored_flags[flag_ndx])
+          g_led_config.flags[led_ndx] += LED_FLAG_KEYLIGHT;
+        if ((1 << (section + BITMASK_INDICATOR)) & stored_flags[flag_ndx])
+          g_led_config.flags[led_ndx] += LED_FLAG_INDICATOR;
+        if (g_led_config.flags[led_ndx] == (LED_FLAG_MODIFIER + LED_FLAG_UNDERGLOW + LED_FLAG_KEYLIGHT + LED_FLAG_INDICATOR)) {
+          g_led_config.flags[led_ndx] = LED_FLAG_ALL;
         }
-        flags_saved = false;
+        //if (section == 4) {
+        //    stored_flags[flag_ndx] = 0;
+        //}
     }
+    //flags_saved = false;
+
     return xap_respond_success(token);
 }
 
